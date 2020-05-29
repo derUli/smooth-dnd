40edf2a519e987e9828102e772aa5f458143197f
Ulrich Schmidt <u.schmidt@velian.de>
2020-05-29 13:31:22 +0200

    changes

--- src/mediator.js
+++ src/mediator.js
@@ -209,7 +209,7 @@ const handleDragStartConditions = (function handleDragStartConditions() {
   let clb;
   let timer = null;
   const moveThreshold = 1;
-  const maxMoveInDelay = 3;
+  const maxMoveInDelay = 10;
 
 
   function onMove(event) {
